92aa66768e3d76f69dd26f0f7375b7f3389799e6
dburnet3 <deniseburnett26@gmail.com>
2020-05-03 16:44:28 -0400

    have the logic working the way I need it to

--- javascript/game.js
+++ javascript/game.js
@@ -1,14 +1,7 @@
 //The array of letter choices
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-//Declaring the number of guesses
-var numGuesses = 0;
 
-//Declaring the number of wins
-var wins = 0;
-
-//Declaring the number of losses
-var losses = 0;
 
 //Declaring the number of guesses left
 var guessesLeft = 9;
@@ -32,7 +25,6 @@ window.onload = function () {
     compuChoice.push(randomLetter);
 
     console.log(randomLetter);
-    console.log(compuChoice);
 }
 
 //on key up event push player Guess into the guesses array
@@ -42,4 +34,38 @@ document.onkeyup = function (event) {
     guesses.push(playerGuess);
 
     console.log(guesses);
+
+
+    if ((playerGuess === compuChoice[0]) && (guessesLeft > 0)) {
+        wins++;
+        guessesLeft = 9;
+        guesses.length = 0;
+        compuChoice.length = 0;
+        var randomLetter = letters[Math.floor(Math.random() * letters.length)];
+        compuChoice.push(randomLetter);
+
+        console.log(randomLetter);
+    }
+    else if ((playerGuess !== compuChoice[0]) && (guessesLeft > 0)) {
+        guessesLeft = guessesLeft - 1;
+    }
+
+    else {
+        losses++;
+        guessesLeft = 9;
+        guesses.length = 0;
+        compuChoice.length = 0;
+        var randomLetter = letters[Math.floor(Math.random() * letters.length)];
+        compuChoice.push(randomLetter);
+        console.log(compuChoice[0]);
+    }
+
+    var html = "<p>Guess what letter I'm thinking of!</p>" +
+        "<p>Wins: " + wins + "</p>" +
+        "<p>Losses: " + losses + "</p>" +
+        "<p>Guesses left: " + guessesLeft + "</p>" +
+        "<p>Your guesses so far: " + guesses + "</p>";
+
+    document.querySelector("#startGame").innerHTML = html;
+
 }
